2c919c3642454ef77a0000739803715c8edfa392
minhhuyphan <phanminhhuycm@gmail.com>
2025-10-05 19:40:11 +0700

    add new file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
+
 const userRoutes = require('./routes/user');
 app.use('/', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
