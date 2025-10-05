d4fc8697b37cebd715f5d16dc602cb4f86ced01a
huynhchiphuc <huynhchiphuchcp@gmail.com>
2025-10-05 19:14:47 +0700

    xong hoat dong 1 2 3

---
+++ backend/controllers/usercontroller.js
@@ -0,0 +1,8 @@
+let users = [];
+
+exports.getUsers = (req, res) => res.json(users);
+exports.createUser = (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
+};
---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/usercontroller');
+
+router.get('/users', userController.getUsers);
+router.post('/users', userController.createUser);
+
+module.exports = router;
