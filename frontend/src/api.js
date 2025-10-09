5b8d130ff45e4154a47d0a98a6096a2682c26126
minhhuyphan <phanminhhuycm@gmail.com>
2025-10-09 14:27:57 +0700

    feat(frontend): add auth UI (login/signup) and styles

---
+++ frontend/src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Request interceptor to add token
+api.interceptors.request.use((config) => {
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (e) {
+    // ignore
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+export default api;
